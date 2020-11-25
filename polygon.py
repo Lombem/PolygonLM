1f87475d3d16dd5dbd179202e08dc018ca53f814
Lombem <lombe.mutale@mail.polimi.it>
2020-11-25 18:30:35 +0100

    Update polygon.py

--- polygon.py
+++ polygon.py
@@ -10,7 +10,6 @@ import math
 
 #Request number of points
 polypts = int(input("Enter the number of polygon points: "))
-print("Your polygon has " + str(polypts) + " polygon points.")
 
 #A list that stores all coordinates - empty to start with
 xcoords = []
@@ -23,5 +22,19 @@ for i in range(polypts):
     xcoords.append(x)
     y = float(input(f"y[{i+1}] "))
     ycoords.append(y)
-print(f'{xcoords} {ycoords}')
+
+#Print a table of entered data and calculated values
+print()
+print(f"{'Point':>5} {'x':>10} {'y':>10}")
+print("-" *43)
+for i in range(polypts):
+    print(f"{i+1:<5} {xcoords[i]:>10.2f} {ycoords[i]:>10.2f}") 
+
+#Calculate the polygon geometry
+Ax = 0
+for i in range(polypts - 1):
+    Ax = Ax + (xcoords[i+1] + xcoords[i]) * (ycoords[i+1] - ycoords[i]) #Area
+print(f"{'Ax:':<5} {0.5*Ax:<10.2f}")
+
+
     
